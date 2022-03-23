b332e240d6959df237b2c91d6d23f05d29c021af
Jamie <jamiejliu8gmail.com>
2022-03-22 22:33:57 -0400

    updated html files

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-// Get the modal
-var modal = document.getElementById("details-modal");
+// Modal for Upcoming Appointments
+function appointmentModal(type, date, time) {
+  var modal = document.getElementById("details-modal");
+  var lnk = document.getElementById("expandModal");
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var lnk = document.getElementById("expandModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-lnk.onclick = function() {
   modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  span.onclick = function() {
     modal.style.display = "none";
+
   }
+
+  window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+    }
+
 }
