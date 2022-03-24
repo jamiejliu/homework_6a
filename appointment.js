8944a3bb2d9db7b68f0ae784c55aa4b3aa96b040
Jamie <jamiejliu8gmail.com>
2022-03-23 22:29:16 -0400

    final code

--- appointment.js
+++ appointment.js
@@ -2,21 +2,19 @@
 function loadAppointments() {
   let upcomingText = document.getElementById("upcomingText");
   var appts = JSON.parse(localStorage.getItem("appts"));
-  let upcomingAppointments = document.getElementById("upcomingAppointments")
+  let upcomingAppointments = document.getElementById("upcomingAppointments");
   console.log(appts);
   if (appts === null) {
     appts = [];
     let noAppointments = document.createElement("p");
     noAppointments.innerHTML = "You have no upcoming appointments scheduled.";
     upcomingAppointments.appendChild(noAppointments);
-    upcomingText.innerHTML = "Upcoming Appointments: 0"
+    upcomingText.innerHTML = "Upcoming Appointments: 0";
   }
 
   else {
-
-    // Display Each Upcoming Appointment
-    let col1 = document.getElementById("upcomingAppointmentsLeft")
-    let col2 = document.getElementById("upcomingAppointmentsRight")
+    let col1 = document.getElementById("upcomingAppointmentsLeft");
+    let col2 = document.getElementById("upcomingAppointmentsRight");
     for (let i=0; i < appts.length; i++) {
       upcomingText.innerHTML = "You have " + appts.length + " upcoming appointment(s).";
       let appt = document.createElement("div");
@@ -24,63 +22,76 @@ function loadAppointments() {
       let type = document.createElement("h3");
       let date = document.createElement("dateText");
       let viewDetails = document.createElement("expandModal");
-      // let details = document.createElement("expandModal");
       type.innerHTML = appts[i].type;
       date.innerHTML = appts[i].date;
       appt.appendChild(type);
       appt.appendChild(date);
       viewDetails.setAttribute("id", "expandModal");
 
-      //Fill in Appointment Details
-      viewDetails.setAttribute("onclick", 'apptModal("${appts[i].type}", "${appts[i].date}, "${appts[i].time}")');
+      // Fill in Appointment Details
+      viewDetails.setAttribute("onclick", `apptModal("${appts[i].type}", "${appts[i].date}", "${appts[i].time}")`);
       viewDetails.classList.add("expandModal");
       viewDetails.innerHTML = "More Details";
       appt.appendChild(viewDetails);
-      // appt.appendChild(details);
       if(i%2 == 0){
-        col1.appendChild(appt)
+        col1.appendChild(appt);
       }
       else {
-        col2.appendChild(appt)
+        col2.appendChild(appt);
       }
-      //upcomingAppointments.appendChild(appt);
     }
   }
 }
 
 // Create Modal for Upcoming Appointments
 function apptModal(type, date, time) {
+
   var modal = document.getElementById("detailsModal");
-  var btn = document.getElementById("expandModal");
-  var span = document.getElementsByClassName("close")[0];
-  let modalType = document.createElement("p");
-  let modalDate = document.createElement("p");
-  let location = document.createElement("p");
-  let modalContent = document.getElementById("modalContent");
-  modalType.innerHTML = // fill
-  modalDate = // fill
-  location.innerHTML = // fill
+  let modalType = document.getElementById("modalType");
+  let modalDate = document.getElementById("modalDate");
+  let modalTime = document.getElementById("modalTime")
+  // let location = document.getElementById("location");
+  // let edit = document.createElement("button")
+  // let notes = document.createElement("p");
+  // let modalContent = document.getElementById('modalContent')
+  // let leftContent = document.getElementById("modalLeft");
+  // let rightContent = document.getElementById("modalRight");
+  modalType.innerHTML = type;
+  modalDate.innerHTML = "Date: " + date;
+  modalTime.innerHTML = "Time: " + time;
+  // location.innerHTML = "Location: E Campus Garage";
+  // notes.innerHTML = "Notes: Please arrive to your appointment within the 15 minute grace period.";
+  // edit.innerHTML = "Edit Appointment";
 
-  modalContent.append(modalType);
-  modalContent.append(modalDate);
-  modalContent.append(location);
+  // modalContent.appendChild(modalType);
+  // modalLeft.appendChild(modalDate);
+  // modalLeft.appendChild(modalTime);
+  // modalLeft.appendChild(edit);
+  // modalRight.appendChild(location);
+  // modalRight.appendChild(notes);
 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+  modal.style.display = "block";
 
 
-  span.onclick = function() {
-    modal.style.display = "none";
-    document.getElementById("modalContent").innerHTML = "";
-    }
+
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  //   document.getElementById("modalContent").innerHTML = "";
+  //   };
 
   window.onclick = function(event) {
   if (event.target == modal) {
       modal.style.display = "none";
-      document.getElementById("modalContent").innerHTML = "";
+      //document.getElementById("modalContent").innerHTML = "";
     }
-  }
+  };
+}
+
+function spanClose(){
+  var modal = document.getElementById("detailsModal");
+  modal.style.display = "none";
+   //document.getElementById("modalContent").innerHTML = "";
 }
 
 loadAppointments();
+
